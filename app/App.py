5bd3dcd5f2a65fefb0c7b1df04d99f233b7bef06
lexiaoau <xiaoleau@gmail.com>
2019-05-05 04:47:15 +1000

    add getUserInput()

--- app/App.py
+++ app/App.py
@@ -7,3 +7,17 @@ class App:
 		self.inputRow = 0
 		self.inputCol = 0
 		self.inputCapacity = 0	# input unit is litre, need to convert to ml
+
+		self.exit = False
+		self.inputStr = ''
+
+	def getUserInput(self):
+		msg1 = 'To calculate j’th glass of the i’th row when K litres are poured into the top most glass, please enter "i j K" in below line(seperated with white space.)'
+		msg2 = 'Or you can enter "exit" to exit this program.'
+		print(msg1)
+		print(msg2)
+
+		self.inputStr = input(">>> ")
+		print('|'+self.inputStr+'|')
+
+
