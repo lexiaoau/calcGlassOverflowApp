f249840b4aa74b692cceba948c05577ffe250c37
lexiaoau <xiaoleau@gmail.com>
2019-05-04 16:47:10 +1000

    add test_glass_overflowToChildren method

--- test/glass_unit_test.py
+++ test/glass_unit_test.py
@@ -11,3 +11,27 @@ class TestGlassClass(unittest.TestCase):
 		self.assertEqual(None, aGlass.rightChild)
 
 		self.assertEqual(Constants.GLASS_CAP, aGlass.capacity)
+
+	def test_glass_fillLiquid(self):
+		aGlass = Glass(None, None)
+		test_overflow = 5
+		aGlass.fillLiquid(Constants.GLASS_CAP + test_overflow)
+		self.assertEqual(test_overflow, aGlass.overflowed)
+
+	def test_glass_overflowToChildren(self):
+		leftChildGlass = Glass(None, None)
+		rightChildGlass = Glass(None, None)
+
+		parentGlass = Glass(leftChildGlass, rightChildGlass)
+
+		self.assertEqual(0, leftChildGlass.filled)
+		self.assertEqual(0, rightChildGlass.filled)
+
+		test_overflow = 5
+		toFill = Constants.GLASS_CAP + test_overflow
+
+		parentGlass.fillLiquid( toFill )
+		parentGlass.overflowToChildren()
+
+		self.assertEqual(test_overflow / 2, leftChildGlass.filled )
+		self.assertEqual(test_overflow / 2, rightChildGlass.filled )
